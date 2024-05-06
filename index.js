daf586c642ec27d7d2edee1d336a076dcf24129a
Muhammad-javed440 <meherjaved440@gmail.com>
2024-05-07 05:44:38 +0600

    Project completed

---
+++ index.js
@@ -0,0 +1,36 @@
+import inquirer from 'inquirer';
+const questions = await inquirer.prompt([
+    {
+        name: "q1",
+        message: "What is the Capital of Pakistan ? ",
+        type: "list",
+        choices: ['Karachi', 'Lahore', 'Islamabad'],
+    },
+    {
+        name: "q2",
+        message: "What is largest city of pakistan by population ?",
+        type: "list",
+        choices: ['Lahore', 'Multan', 'Karachi', 'Islamabad'],
+    }
+]);
+let answer1 = "Islamabad";
+let answer2 = "Karachi";
+let score = 0;
+if (questions.q1 === answer1) {
+    console.log("Corret answer .", answer1);
+    score++;
+    console.log('Your score is:', score);
+}
+else {
+    console.log('Wrong answer.');
+}
+;
+if (questions.q2 === answer2) {
+    console.log("Corret answer .", answer2);
+    score++;
+    console.log('Your score is:', score);
+}
+else {
+    console.log('Wrong answer.');
+}
+;
